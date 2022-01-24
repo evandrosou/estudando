737688bde03ffdad609d92bd16a95111c59dc5f5
user <evandrosou@outlook.com>
2022-01-24 20:14:15 -0300

    meus estudos

--- nome_sobrenome.py
+++
@@ -1,6 +0,0 @@
-print("Este programa exibira seu nome completo")
-nome = input("digite o seu nome:")
-sobrenome = input("Digite seu sobrenome:")
-
-nome_completo = nome + " " + sobrenome
-print("O nome completo é: ",nome_completo)
--- velocidade_media.py
+++
@@ -1,8 +0,0 @@
-velocidade = float(input("\nDigite a velocidade objeto movel:"))
-distancia = float(input("Digite a distancia percorrida:"))
-
-Calculo = 0
-
-calculo = velocidade / distancia
-
-print("O tempo de percurso será: {}".format(calculo))
